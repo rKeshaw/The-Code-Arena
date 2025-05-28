4da9763e9e0498a8b2f7fda6af3a28a07ea2cb68
rKeshaw <101080156+rKeshaw@users.noreply.github.com>
2025-05-28 20:18:03 +0530

    Create w1_q1.cpp
    //

---
+++ Keshaw/Week1/w1_q1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+typedef long long ll;
+using namespace std;
+
+int main(void){
+	ll len;
+	cin >> len;
+	bool arr[len] = {0};
+	ll num;
+	for (ll i=0; i<len-1; i++){
+		cin >> num;
+		arr[num-1] = 1;
+	}
+	for (ll i=0; i<len; i++){
+		if (arr[i] == 0){
+			cout << i+1 << "\n";
+			break;
+		}
+	}
+}
